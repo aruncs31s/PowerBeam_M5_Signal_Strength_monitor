375b2fc65f62c72f1646eccbc148c36e833e71d0
aruncs31s <aruncs31ss@gmail.com>
2024-07-05 13:14:02 +0100

    Backup from Raspberry Pi at Fri  5 Jul 13:14:02 BST 2024

--- src/Arduino/arduino_raspberry_communication/raspberry_main.py
+++ src/Arduino/arduino_raspberry_communication/raspberry_main.py
@@ -1,9 +1,9 @@
 import serial
 import time
-ser = serial.Serial('/dev/ttyAMA0' , 9600) 
 
 try: 
     while True: 
+        ser = serial.Serial('/dev/ttyAMA0' , 9600) 
         if ser.in_waiting> 0:
             data = ser.readline().decode('utf-8').strip()
             print(f"{data}")

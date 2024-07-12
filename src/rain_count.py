a01c5bc8fa4dd9379fa65e68a5b5038188fa37d9
aruncs31s <aruncs31ss@gmail.com>
2024-07-12 20:33:43 +0530

    Backcup Fri Jul 12 08:33:43 PM IST 2024

--- src/rain_count.py
+++ src/rain_count.py
@@ -1,17 +1,13 @@
-from os import error
 import serial
 import time
 
-rain_sensor = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+rain_sensor = serial.Serial("/dev/ttyAMA0", 9600, timeout=2)
 
-
-def rain_count():
-    try:
+try:
+    while True:
         if rain_sensor.in_waiting > 0:
             data = rain_sensor.readline().decode("utf-8").strip()
             print(f"{data}")
-    except error as e:
-        print(e)
-
-
-rain_count()
+            exit(0)
+except KeyboardInterrupt:
+    rain_sensor.close()

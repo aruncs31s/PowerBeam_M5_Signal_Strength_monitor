889b2bd16bdd62c8c6c310d3ed6b9467386f0d71
aruncs31s <aruncs31ss@gmail.com>
2024-07-13 05:25:46 +0530

    Backcup Sat Jul 13 05:25:46 AM IST 2024

--- src/main.py
+++ src/main.py
@@ -4,3 +4,5 @@ import humidity_temperature as dht
 rain_count = rain.count()
 temp = dht.temp()
 humidity = dht.humidity()
+
+print(f"rain_count = {rain_count} temperature {temp} humidity = {humidity}")

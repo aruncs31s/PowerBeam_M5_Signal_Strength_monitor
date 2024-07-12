5ea2800ce88ba298dc6fe121e567c397cd289b85
aruncs31s <aruncs31ss@gmail.com>
2024-07-13 05:20:20 +0530

    Backcup Sat Jul 13 05:20:20 AM IST 2024

--- src/humidity_temperature.py
+++ src/humidity_temperature.py
@@ -1,5 +1,11 @@
 # Author : Arun CS
 # Github : https://github.com/aruncs31s/
+
+"""
+Note That i've used Raspberry pi 2 b for this testing and when i try to connect DHT11 to GPIO4 and read it doesnt work but somehow by connecting the sensor to GPIO7 and passing 4 to the read_retry() function works
+
+"""
+
 import sys
 import Adafruit_DHT
 

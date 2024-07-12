d248f5ed595efc50ee15d1fe92ed0960c5f4d685
aruncs31s <aruncs31ss@gmail.com>
2024-07-12 19:50:07 +0530

    Backcup Fri Jul 12 07:50:07 PM IST 2024

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from rain_count import rain_count
+
+rain_count()
---
+++ src/rain_count.py
@@ -0,0 +1,14 @@
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyAMA0", 9600)
+
+
+def rain_count():
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                data = ser.readline().decode("utf-8").strip()
+               print(f"{data}")
+    except KeyboardInterrupt:
+        ser.close()

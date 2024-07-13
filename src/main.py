b78d18dfa2513b2eb4fa075aead304e5add7d729
aruncs31s <aruncs31ss@gmail.com>
2024-07-13 06:51:44 +0530

    Backcup Sat Jul 13 06:51:44 AM IST 2024

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Assuming the subfolder is named 'subfolder' and located in the same directory as this script
+subfolder_path = os.path.join(os.path.dirname(__file__), "lib")
+sys.path.append(subfolder_path)
+
 from logging import error
 import rain
 import humidity_temperature as dht

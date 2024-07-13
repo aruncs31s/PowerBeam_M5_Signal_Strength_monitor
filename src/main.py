37ae93d0c4d7759f80621b2a24150f514d2a21b6
aruncs31s <aruncs31ss@gmail.com>
2024-07-13 05:43:50 +0530

    Backcup Sat Jul 13 05:43:50 AM IST 2024

--- src/main.py
+++ src/main.py
@@ -78,7 +78,7 @@ while True:
         signal_value = get_signal_value()
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(
-            f"{timestamp} - Signal value: {signal_value} dBm , Rain_Count: {rain_count} Humidity: {dht.humidity()} Rain Status: {rain.compare()}"
+            f"{timestamp} - Signal value: {signal_value} dBm , Rain_Count: {rain.get_count()} Humidity: {dht.humidity()} Rain Status: {rain.compare()}"
         )
         # Save to file
         with open(output_file, "a") as f:

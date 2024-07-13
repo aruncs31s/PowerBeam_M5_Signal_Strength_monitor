ae267c31cde63b43f9f329aba96ffaa352ad4006
aruncs31s <aruncs31ss@gmail.com>
2024-07-13 08:44:14 +0530

    Backcup Sat Jul 13 08:44:14 AM IST 2024

--- src/lib/rain.py
+++ src/lib/rain.py
@@ -4,20 +4,22 @@
 
 import serial
 
-rain_sensor = serial.Serial("/dev/ttyAMA0", 9600, timeout=15)
+rain_sensor = serial.Serial("/dev/ttyAMA0", 9600, timeout=2)
 
 
 def get_count():
     try:
         while True:
             if rain_sensor.in_waiting > 0:
+
                 return int(rain_sensor.readline().decode("utf-8").strip())
+                exit(0)
     except KeyboardInterrupt:
         rain_sensor.close()
 
 
 def compare(rain_count_previous):
     if rain_count_previous == get_count():
-        return False
-    else:
         return True
+    else:
+        return False
